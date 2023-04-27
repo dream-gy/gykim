8e78f7fb64b8805f1994af6c68a85d1090a1e953
gygygy <74412967+dream-gy@users.noreply.github.com>
2023-04-28 04:35:17 +0900

    Add files via upload

--- js/sub.js
+++ js/sub.js
@@ -1,117 +1,3 @@
-//주소창 파라미터 값 가져오기
-function getParameterByName(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-      results = regex.exec(location.href);
-  return results == null ? "": decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-
-
-
-$(function(){
-  
-  //header addClass & removeClass
-  $(window).scroll(function(){
-    var hegiht = $("main").height()/4;
-    if($(window).scrollTop() > 1){
-      $("header").addClass("affix")
-    }else {
-      $("header").removeClass("affix")
-    }
-  })//window.scroll
-
-  //반응형에 맞게 spliiter변경하기
-  var tablet = 768;
-  var timer = null;
-  var spliiter;
- 
-  function splitterResize(){
-    var sptHeight;
-    var sptOri;
-    if($(window).width() > tablet ){
-      sptHeight = 500;
-      sptOri = "vertical";
-      splitterChange(sptHeight,sptOri);
-    }else {
-      sptHeight = 600;
-      sptOri = "horizontal";
-      splitterChange(sptHeight,sptOri);
-    }
-  }//splitterResize
-  splitterResize();
-
-  function splitterChange(sptHeight, sptOri){
-    $(".detail-list>li").each(function(){
-      var contId = $(this).attr("id");
-      spliiter = $("#"+contId+"").find(".cont").height(sptHeight).split({
-        orientation: sptOri,
-        limit: 100
-      });
-    });
-  }//splitterChange
- 
-  $(window).resize(function(){
-    splitterResize();
-    clearTimeout(timer);
-    timer = setTimeout(function() {
-     location.reload(); //리로드하기
-     sizeLayout() //size 레아이웃 바꾸기
-     }, 250);
-   });
-
-  var tit = $(".detail-list h4");
-  var skillUl = $(".skills .cont");
-  
-  //skills리스트로 나타내기
-  tit.each(function(idx){
-    var titName = $(this).text();
-    var skillList = "<li>"+titName +"</li>";
-    skillUl.append(skillList);
-
-    //형광펜 스크롤 이벤트
-    var offsetTop =  $(this).offset().top;
-    $(window).scroll(function(){
-      if($(this).scrollTop() > offsetTop - 500){
-        tit.eq(idx).addClass("on");
-      }else {
-        tit.eq(idx).removeClass("on");
-      }
-    })//window.scroll
-  });//tit.each
-
- 
-  //skills.click 해당 부분 스크롤 이벤트
-  skillUl.on("click","li",function(){
-    var thisidx = $(this).index();
-    var offsetTop = tit.eq(thisidx).offset().top;
-    $("html,body").animate({
-      scrollTop: offsetTop - 150,
-    },500);
-  }) //skills.click
-
-
-
-
-  //scrollTop 애니메이션
-  $(".scroll-top").click(function(){
-    $("html,body").animate({
-      scrollTop: 0,
-    },500);
-  })//scroll-top.click
-
-
-  //size 레아이웃 바꾸기
-  var ogLayout = $(".info-list>li").eq(3).find(".cont").html();
-  var chgLayout = ogLayout.replace(/:/g,":<br>");
-
-  function sizeLayout(){
-    if($(window).width() < tablet ){
-        $(".info-list>li").eq(3).find(".cont").html(chgLayout);
-    }
-  }//sizeLayout
-  sizeLayout() 
-
-})//function.ready
 
 $.ajax({
   url : "./data/item.json",
@@ -126,8 +12,12 @@ $.ajax({
         page,
         size,
         html,
-        linkhtml,
-        elements =[];
+        linkhtm;
+    var elements =[];
+
+
+
+
     $.each(data,function(index, item){
       if(itemId == item.id){
           mainImg = item.mainImage;
@@ -151,7 +41,7 @@ $.ajax({
                   "<h4>"+list.subtitle+"</h4>"+
                   "<div class='desc'>" + list.subcont +  
                     "<p>관련링크</p>" +
-                    "<ul>"+arrlinke+"</ul>"+
+                    "<ul class='related-links'>"+arrlinke+"</ul>"+
                   "</div>"+
                   "<div class='cont'>"+
                     "<div class='img'><img src='"+list.image.url01+"' alt=''></div>"+
@@ -186,6 +76,187 @@ $.ajax({
     $(".info-list li").eq(3).find(".cont").html(size);
     $(".info-list li").eq(4).find(".cont").html(page);
     $(".detail-list").append(elements);
-          
+
+   
+    var tit = $(".detail-list h4");
+    var skillUl = $(".skills .cont");
+    //skills리스트로 나타내기
+    tit.each(function(idx){
+      var titName = $(this).text();
+      var skillList = "<li>"+titName +"</li>";
+      skillUl.append(skillList);
+
+    });//tit.each
+
+    //형광펜 스크롤 이벤트
+    $(window).scroll(function(){
+      for(let i = 0; i < tit.length; i++){
+        var offsetTop =  tit.eq(i).offset().top;
+        var offsetBottom = offsetTop + tit.eq(i).outerHeight();
+        console.log(offsetTop)
+        if( $(this).scrollTop() >= offsetTop - 400  ){
+          tit.eq(i).addClass("on");
+        }else {
+          tit.eq(i).removeClass("on");
+        }
+      }
+    })//window.scroll
+    
+
+    //skills.click 해당 부분 스크롤 이벤트
+    skillUl.on("click","li",function(){
+      var thisidx = $(this).index();
+      var offsetTop = tit.eq(thisidx).offset().top;
+      $("html,body").animate({
+        scrollTop: offsetTop - 150,
+      },500);
+    }) //skills.click
+
+
+    //반응형에 맞게 spliiter변경하기
+    var tablet = 768;
+    var timer = null;
+    var spliiter;
+
+    function splitterResize(){
+      var sptHeight;
+      var sptOri;
+      if($(window).width() > tablet ){
+        sptHeight = 500;
+        sptOri = "vertical";
+        splitterChange(sptHeight,sptOri);
+      }else {
+        sptHeight = 600;
+        sptOri = "horizontal";
+        splitterChange(sptHeight,sptOri);
+      }
+    }//splitterResize
+    splitterResize();
+
+    function splitterChange(sptHeight, sptOri){
+      $(".detail-list>li").each(function(){
+        var contId = $(this).attr("id");
+        spliiter = $("#"+contId+"").find(".cont").height(sptHeight).split({
+          orientation: sptOri,
+          limit: 100
+        });
+      });
+    }//splitterChange
+
+    $(window).resize(function(){
+      splitterResize();
+      clearTimeout(timer);
+      timer = setTimeout(function() {
+      location.reload(); //리로드하기
+      sizeLayout() //size 레아이웃 바꾸기
+      }, 250);
+    });
+
+    //파라미터에 cont문자열이 포함되면 해당레이어로 이동하기
+    var PageId = getPageId();
+ 
+    if(PageId.indexOf("cont") != -1){
+      // moveCont()
+       
+    }//if(PageId.indexOf("cont") != -1)
+
+     //related-links(관련링크) 클릭 이벤트 
+  $(".related-links a").on("click", function(e){
+    //  e.preventDefault();
+    var linkValue = $(this).attr("href");
+        linkValue = linkValue.replace( /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/gi,"");
+   
+      console.log(linkValue.indexOf("itemId")) 
+      if(linkValue.indexOf("itemId") == -1){
+        e.preventDefault();
+          aniCont(linkValue);
+      }else {
+        location.href = linkValue;
+      }
+  })//$(".related-links a").click
+
+  var PageId = getPageId();
+  if(PageId.indexOf("cont") != -1){
+    console.log(PageId)
+      aniCont(PageId)
+    
+    }//if(PageId.indexOf("cont") != -1)
   }//data.success
 });//ajax
+
+
+
+
+//주소창 파라미터 값 가져오기
+function getParameterByName(name) {
+  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+      results = regex.exec(location.href);
+  return results == null ? "": decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function getPageId() {
+  var pageUrl = window.location.href;
+  var CurrentPageId = pageUrl.substring(pageUrl.lastIndexOf("#") + 1);
+  return CurrentPageId;
+}
+function moveCont(linkValue){
+ 
+
+}
+
+
+
+
+function aniCont(Value){
+  var cont = $(".detail-list").find("#"+Value).offset().top;
+  var headerH = $("header").height();
+  $("html,body").animate({
+     scrollTop: cont - headerH,
+   },500);
+
+}
+
+
+
+
+$(function(){
+
+ 
+  //header addClass & removeClass
+  $(window).scroll(function(){
+    var hegiht = $("main").height()/4;
+    if($(window).scrollTop() > 1){
+      $("header").addClass("affix")
+    }else {
+      $("header").removeClass("affix")
+    }
+  })//window.scroll
+
+
+  //scrollTop 애니메이션
+  $(".scroll-top").click(function(){
+    $("html,body").animate({
+      scrollTop: 0,
+    },500);
+  })//scroll-top.click
+
+
+  //size 레아이웃 바꾸기
+  var tablet = 768;
+  var ogLayout = $(".info-list>li").eq(3).find(".cont").html();
+  var chgLayout = ogLayout.replace(/:/g,":<br>");
+
+  function sizeLayout(){
+    if($(window).width() < tablet ){
+        $(".info-list>li").eq(3).find(".cont").html(chgLayout);
+    }
+  }//sizeLayout
+  sizeLayout() 
+
+
+ 
+ 
+
+})//function.ready
+
